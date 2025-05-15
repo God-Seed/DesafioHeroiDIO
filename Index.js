3ed5cb622b430abc84fc76c3b63dbea165a45be0
God-Seed <juniorvalmir2010@hotmail.com>
2025-05-14 23:10:51 -0300

    Meu primeiro commit

--- Index.js
+++ Index.js
@@ -1,43 +1,32 @@
 let nomeHeroi = "SidTheFenrir"
 let nivelHeroi = 0
 
-do {nivelHeroi++;
-switch (nivelHeroi){
-
-case nivelHeroi <= 1000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Ferro")
-break
-
-case nivelHeroi = 1001 >= 2000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Bronze")
-break
-
-case nivelHeroi <= 2001 >= 5000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Prata")
-break
-
-case nivelHeroi <= 5001 >= 7000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Ouro")
-break
-
-case nivelHeroi <= 7001 >= 8000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Platina")
-break
-
-case nivelHeroi <= 8001 >= 9000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Ascendente")
-break
-
-case nivelHeroi <= 9001 >= 10000:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Imortal")
-break
-
-case nivelHeroi >= 10001:
-console.log ("O Herói de nome" + nomeHeroi +" está no nível de Radiante")
-break
-}} while (nivelHeroi < 10003);
-
-
+for (let nivelHeroi = 1; nivelHeroi < 11000; nivelHeroi = nivelHeroi + 1000) {
+    if (nivelHeroi <= 1000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+    }
+    else if (nivelHeroi >= 1001 && nivelHeroi <= 2000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+    }
+    else if (nivelHeroi >= 2001 && nivelHeroi <= 5000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+    }
+    else if (nivelHeroi >= 5001 && nivelHeroi <= 7000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+    }
+    else if (nivelHeroi >= 7001 && nivelHeroi <= 8000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+    }
+    else if (nivelHeroi >= 8001 && nivelHeroi <= 9000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+    }
+    else if (nivelHeroi >= 9001 && nivelHeroi <= 10000) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+    }
+    else if (nivelHeroi >= 10001) {
+        console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+    }
+}
 
 //Se XP for menor do que 1.000 = Ferro
 //Se XP for entre 1.001 e 2.000 = Bronze
